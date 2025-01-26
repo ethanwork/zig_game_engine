0cd06a16434168fc3243e080483b4e708bed483e
Ethan <ethanw.work@outlook.com>
2025-01-25 21:49:59 -0800

    changes

--- src/zig_examples/enums_and_taggedunions.zig
+++ src/zig_examples/enums_and_taggedunions.zig
@@ -6,7 +6,42 @@ const Color = enum {
     Blue,
 };
 
+// assign custom integer values to an enum
+const HttpStatusCodes = enum(u16) {
+    OK = 200,
+    NotFound = 404,
+    InternalServerError = 500,
+};
+
+const Shape = union(enum) {
+    Circle: f64,
+    Rectangle: struct {
+        width: f64,
+        height: f64,
+    },
+};
+
 pub fn enum_examples() void {
-    const color = Color.Red;
+    const color = Color.Blue;
     std.debug.print("color = {}\n", .{color});
+    std.debug.print("color int value = {}\n", .{@intFromEnum(color)});
+
+    const circle = Shape{ .Circle = 3.0 };
+    const rectangle = Shape{ .Rectangle = .{ .width = 2.0, .height = 3.0 } };
+    switch (circle) {
+        .Circle => |radius| {
+            std.debug.print("circle radius = {}\n", .{radius});
+        },
+        .Rectangle => |rect| {
+            std.debug.print("rectangle width = {}, height = {}\n", .{rect.width, rect.height});
+        },
+    }
+    switch (rectangle) {
+        .Circle => |radius| {
+            std.debug.print("circle radius = {}\n", .{radius});
+        },
+        .Rectangle => |rect| {
+            std.debug.print("rectangle width = {}, height = {}\n", .{rect.width, rect.height});
+        },
+    }
 }
